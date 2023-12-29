6d5ff2051ca488306e3ad9655e7c437c1b235997
GustavM01 <gustav.milihagulu@gmail.com>
2023-12-29 18:16:32 +0100

    Lägga till todos

--- TodoAPI.Tests/UnitTest1.cs
+++ TodoAPI.Tests/UnitTest1.cs
@@ -1,11 +1,53 @@
+using TodoAPI.Controllers;
+using TodoAPI.Data;
+using TodoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace TodoAPI.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private TodoContext _context;
 
+        public UnitTest1()
+        {
+            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase("InMemory").Options;
+            _context = new TodoContext(options);
+        }
+
+        // Delete info
+        private Task ResetContext()
+        {
+            _context.Todo.RemoveRange(_context.Todo);
+            _context.SaveChangesAsync();
+
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public void Add_Todo_Check_if_saved_in_db()
+        {
+            // Arrange
+            var todo = new Todo 
+            { 
+                Id = 111, 
+                Text = "Eat", 
+                IsDone = false 
+            };
+
+            // Service to handle requests
+            var service = new TodoService(_context);
+
+            // Act
+            var result = service.PostNote(todo);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(todo.Text, result.Text);
+            Assert.Equal(todo.IsDone, result.IsDone);
+
+            // Assert that the todo is saved in the database
+            _context.Todo.Single(x => x.Text == todo.Text);
         }
     }
 }
---
+++ TodoAPI/Controllers/TodoService.cs
@@ -0,0 +1,30 @@
+﻿using TodoAPI.Data;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    public class TodoService
+    {
+        private TodoContext _context;
+
+        public TodoService(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public Todo PostNote(Todo todo)
+        {
+            if (string.IsNullOrWhiteSpace(todo.Text) || todo is null)
+            {
+                throw new NullReferenceException("Invalid todo");
+            }
+
+            Todo newTodo = new Todo { Text = todo.Text, IsDone = todo.IsDone };
+            _context.Todo.Add(newTodo);
+            _context.SaveChanges();
+
+            return newTodo;
+        }
+
+    }
+}
